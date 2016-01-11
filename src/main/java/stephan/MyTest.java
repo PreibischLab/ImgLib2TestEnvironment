27df047783f8b91d2af73789db032bc2960377e1
Stephan Preibisch <stephan.preibisch@gmx.de>
2016-01-11 14:54:55 +0100

    Example of how a cursor iterates a CellImg

--- src/main/java/stephan/MyTest.java
+++ src/main/java/stephan/MyTest.java
@@ -1,9 +1,29 @@
 package stephan;
 
+import net.imglib2.Cursor;
+import net.imglib2.img.Img;
+import net.imglib2.img.cell.CellImgFactory;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.real.FloatType;
+
 public class MyTest
 {
-	public void test()
+	public static void iterateAllPixels()
 	{
+		Img< FloatType > test = new CellImgFactory< FloatType >( 3 ).create( new int[]{ 10, 10 }, new FloatType() );
 		
+		final Cursor< FloatType > c = test.cursor();
+		
+		int i = 0;
+		
+		while (  c.hasNext() )
+		{
+			c.fwd();
+			c.get().set( i );
+			++i;
+		}
+		
+		ImageJFunctions.show( test );
+
 	}
 }
