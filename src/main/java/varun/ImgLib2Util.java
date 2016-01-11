1b0e58547da017443b98f7a8f0714da672aa4ad8
Stephan Preibisch <stephan.preibisch@gmx.de>
2016-01-11 16:41:12 +0100

    fixed two bugs

--- src/main/java/varun/ImgLib2Util.java
+++ src/main/java/varun/ImgLib2Util.java
@@ -1,4 +1,4 @@
-package util;
+package varun;
 
 import ij.ImageJ;
 import ij.ImagePlus;
@@ -28,7 +28,7 @@ public static Img<FloatType> Copy32Bit(final Img< FloatType > img1)
 final Img< FloatType > img2;
 
 
-			img2 = factory.create( img1, new FloatType ());
+			img2 = img1.factory().create( img1, new FloatType ());
 
 
 final Cursor< FloatType > c1 = img1.localizingCursor();
