40955a0c4528591b031cb2ee76c913bfc48f6815
Stephan Preibisch <stephan.preibisch@gmx.de>
2016-01-12 15:53:59 +0100

    formatted source

--- src/main/java/varun/Sphere.java
+++ src/main/java/varun/Sphere.java
@@ -1,7 +1,5 @@
 package varun;
 
-
- 
 import ij.ImageJ;
 import net.imglib2.Cursor;
 import net.imglib2.Point;
@@ -13,51 +11,47 @@ import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 
-public class Sphere {
-
-	public static <T extends RealType<T>> void drawSpheres(final RandomAccessibleInterval<T>img  ){
-		
-		  // the number of dimensions
-      int numDimensions = img.numDimensions();
-long[] center = new long[img.numDimensions()];
-for(int d=0; d<img.numDimensions();++d)
+public class Sphere
 {
-	center[d]=(img.max(d)-img.min(d))/2+img.min(d);
-	
-}
-      // define the center and radius
-      Point centerdef = new Point( center );
-      long minSize = img.dimension( 0 );
-      long radius=10;
-      HyperSphere<T> testsphere= new HyperSphere<T>(img, centerdef, radius);
-      
-Cursor<T> c = testsphere.cursor();
 
-long var=0;
+	public static < T extends RealType< T >> void drawSpheres(
+			final RandomAccessibleInterval< T > img )
+	{
 
-while(c.hasNext()){
-c.fwd();
-c.get().setReal(var);
-++var;
+		// the number of dimensions
+		long[] center = new long[ img.numDimensions() ];
+		for (int d = 0; d < img.numDimensions(); ++d)
+		{
+			center[ d ] = ( img.max( d ) - img.min( d ) ) / 2 + img.min( d );
+		}
 
+		// define the center and radius
+		Point centerdef = new Point( center );
+		long radius = 10;
+		HyperSphere< T > testsphere = new HyperSphere< T >( img, centerdef,
+				radius );
 
-}
+		Cursor< T > c = testsphere.cursor();
 
+		long var = 0;
+
+		while ( c.hasNext() )
+		{
+			c.fwd();
+			c.get().setReal( var );
+			++var;
+
+		}
 
 	}
-	
-	
 
-
-	
-	
-	public static void main (String[] args){
+	public static void main( String[] args )
+	{
 		new ImageJ();
-		
-		final Img<FloatType> img =ArrayImgs.floats(100,100);
-		drawSpheres(img);
+
+		final Img< FloatType > img = ArrayImgs.floats( 100, 100 );
+		drawSpheres( img );
 		ImageJFunctions.show( img );
 	}
-		
-}
 
+}
