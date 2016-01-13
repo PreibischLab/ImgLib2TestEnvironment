3bf21657b5e67f48778d25208c55a77ff6f04d46
Stephan Preibisch <stephan.preibisch@gmx.de>
2016-01-13 16:14:15 +0100

    added MeanFilter, Thresholding, Distance Transform

---
+++ src/main/java/stephan/DistanceTransform.java
@@ -0,0 +1,11 @@
+package stephan;
+
+/**
+ * Based on a binary image (from thresholding), compute for every bright pixel the closest distance to a 0-pixel 
+ * 
+ *
+ */
+public class DistanceTransform
+{
+
+}
---
+++ src/main/java/stephan/Thresholding.java
@@ -0,0 +1,13 @@
+package stephan;
+
+/**
+ * 
+ * For every pixel see if it is above or below a threshold
+ * 
+ * if above, set 1, otherwise 0 > use a binary image for this (BitType)
+ *
+ */
+public class Thresholding
+{
+
+}
