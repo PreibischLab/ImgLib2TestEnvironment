8a4d6250d438d18a40f05551ca201651037c1afe
kapoorlab <kapoorconsciousnessgmail.com>
2016-01-14 18:40:01 +0100

    Thresholding and Distance transform in one code (primitive style implementation).

--- src/main/java/varun/distanceTransform.java
+++ src/main/java/varun/distanceTransform.java
@@ -5,21 +5,22 @@ import java.io.File;
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.algorithm.binary.Thresholder;
+
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgFactory;
-import net.imglib2.img.array.ArrayImgs;
+
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.logic.BitType;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.view.Views;
 
 public class distanceTransform {
 
-	public static void computeDistance(RandomAccessibleInterval<BitType> img,
-			RandomAccessibleInterval<BitType> imgout) {
+	public static void createBitimage(RandomAccessibleInterval<FloatType> img, RandomAccessibleInterval<BitType> imgout,
+			FloatType ThresholdValue) {
 
-		final Cursor<BitType> bound = Views.iterable(img).cursor();
+		final Cursor<FloatType> bound = Views.iterable(img).localizingCursor();
 
 		final RandomAccess<BitType> outbound = imgout.randomAccess();
 
@@ -29,69 +30,84 @@ public class distanceTransform {
 
 			outbound.setPosition(bound);
 
-			
-			
-			if (bound.get().getInteger() == 0) {
+			if (bound.get().compareTo(ThresholdValue) > 0) {
 
-				double distance=0;
-				double mindistance=0;
-				double Huge=10;
-				
-				
-				final Cursor<BitType> second = Views.iterable(img).cursor();
+				outbound.get().setOne();
 
-				while (second.hasNext()) {
-
-					second.fwd();
-
-					if (second.get().getInteger() == 1) {
-						
-						
-						
-						for(int i=0; i<second.numDimensions();++i){
-							
-						distance+=	Math.abs(bound.getIntPosition(i)-second.getIntPosition(i));
-						}
-						
-					//	System.out.println(distance);
-
-						// Compute mindistance somehow!
-
-					}
-					
-					mindistance=Math.min(distance, Huge);
-
-				}
-
-//				mindistance=??
-				
-				outbound.get().setReal(mindistance);
-				
 			}
 
-			
-			
+			else {
+
+				outbound.get().setZero();
+
+			}
+
+		}
+	}
+
+	public static <T extends RealType<T>> void computeDistance(RandomAccessibleInterval<BitType> img,
+			RandomAccessibleInterval<T> imgout) {
+
+		final Cursor<BitType> bound = Views.iterable(img).cursor();
+
+		final RandomAccess<T> outbound = imgout.randomAccess();
+
+		while (bound.hasNext()) {
+			bound.fwd();
+			outbound.setPosition(bound);
+
+		
+			if (bound.get().getInteger() == 0) {
+				final Cursor<BitType> second = Views.iterable(img).cursor();
+				double mindistance = 1.0E300;
+				
+
+				double distance = 0;
+				while (second.hasNext())
+					if (second.next().getInteger() == 1)
+
+						for (int d = 0; d < second.numDimensions(); ++d) {
+
+							distance += Math.pow((second.getDoublePosition(d) - bound.getDoublePosition(d)), 2);
+
+						}
+
+				mindistance = Math.min(mindistance, Math.sqrt(distance));
+				
+				System.out.println(mindistance);
+
+				outbound.get().setReal(mindistance);
+			}
+
+			else {
+				outbound.get().setReal(0);
+			}
+
 		}
 
 	}
 
 	public static void main(String[] args) {
-		
-		//final Img< FloatType > img=ArrayImgs.floats(100,100);
+
 		final Img<FloatType> img = ImgLib2Util.openAs32Bit(new File("src/main/resources/dt.png"));
-		
-		final Img< BitType > bitimg = Thresholder.threshold( img, new FloatType( 20 ), true, 1 );
-		
-		final Img< BitType>bitimgout= new ArrayImgFactory<BitType>().create(img, new BitType());
-		
-		
-		
-		
-		computeDistance(bitimg,bitimgout);
-		ImageJFunctions.show(img);
-		//ImageJFunctions.show(bitimg);
-		ImageJFunctions.show(bitimgout);
-		
+
+		ImageJFunctions.show(img).setTitle("Original_Image");
+
+		final Img<BitType> imgout = new ArrayImgFactory<BitType>().create(img, new BitType());
+
+		final Img<BitType> bitimgout = new ArrayImgFactory<BitType>().create(img, new BitType());
+
+		FloatType val = new FloatType(200);
+
+		createBitimage(img, imgout, val);
+
+		computeDistance(imgout, img);
+
+		ImageJFunctions.show(img).setTitle("FloatType_output");
+
+		computeDistance(imgout, bitimgout);
+
+		ImageJFunctions.show(bitimgout).setTitle("BitType_output");
 
 	}
 
